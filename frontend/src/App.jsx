6028c96b415b09937c61e495b233b4e334a7b5db
gvfirmeza <pmvieira@outlook.com>
2023-05-25 00:08:33 -0300

    put e post

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,23 +21,86 @@ const FuncDelete = () => {
     let id = document.getElementById("getinput").value;
     axios.get(`http://localhost:8080/medicos/` + id)
       .then(response => {
-        setResponseData(response.data); 
+        setResponseData(response.data);
+
+        setUpdateData({
+          id: response.data.id,
+          nome: response.data.nome,
+          email: response.data.email
+        });
+
       })
       .catch(error => {
         console.error(error);
       });
   };
 
+  const [updateData, setUpdateData] = useState({
+    id: '',
+    nome: '',
+    email: ''
+  });
+
+  const handleUpdate = () => {
+    axios.put(`http://localhost:8080/medicos`, updateData)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  const handleChange = event => {
+    const { name, value } = event.target;
+    setUpdateData(prevData => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
+
+  const [createData, setCreateData] = useState({
+    nome: '',
+    email: '',
+    crm: '',
+    especialidade: ''
+  });
+
+  const handleCreate = () => {
+    axios.post('http://localhost:8080/medicos', createData)
+      .then(response => {
+        console.log(response);
+        // Limpar os campos do formulário após a criação bem-sucedida
+        setCreateData({
+          nome: '',
+          email: '',
+          crm: '',
+          especialidade: ''
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  const handleCreateChange = event => {
+    const { name, value } = event.target;
+    setCreateData(prevData => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
+
   return (
-      <body className="App-main">
+    <body className="App-main">
 
-        <input id="deleteinput" type="number"></input>
-        <button onClick={handleDelete}>Delete</button>
+      <h2>Listar Médico</h2>
+      <input id="getinput" type="number"></input>
+      <button onClick={handleGet}>Get</button>
 
-        <input id="getinput" type="number"></input>
-        <button onClick={handleGet}>Get</button>
-        
-        {responseData && (
+      <br />
+
+      {responseData && (
         <div>
           <p>ID: {responseData.id}</p>
           <p>Nome: {responseData.nome}</p>
@@ -45,9 +108,39 @@ const FuncDelete = () => {
           <p>CRM: {responseData.crm}</p>
           <p>Especialidade: {responseData.especialidade}</p>
         </div>
-        )}
+      )}
 
-      </body>
+      <br />
+
+      <h2>Atualizar Médico</h2>
+      <label>ID: </label>
+      <input id="updateId" type="number" name="id" value={updateData.id} onChange={handleChange} readOnly></input><br />
+      <label>Nome: </label>
+      <input type="text" name="nome" value={updateData.nome} onChange={handleChange}></input><br />
+      <label>Email: </label>
+      <input type="text" name="email" value={updateData.email} onChange={handleChange}></input>
+      <button onClick={handleUpdate}>Update</button>
+
+      <br />
+
+      <h2>Deletar Médico</h2>
+      <input id="deleteinput" type="number"></input>
+      <button onClick={handleDelete}>Delete</button>
+
+      <br />
+
+      <h2>Criar Médico</h2>
+      <label>Nome: </label>
+      <input type="text" name="nome" value={createData.nome} onChange={handleCreateChange}></input><br />
+      <label>Email: </label>
+      <input type="text" name="email" value={createData.email} onChange={handleCreateChange}></input><br />
+      <label>CRM: </label>
+      <input type="text" name="crm" value={createData.crm} onChange={handleCreateChange}></input><br />
+      <label>Especialidade: </label>
+      <input type="text" name="especialidade" value={createData.especialidade} onChange={handleCreateChange}></input>
+      <button onClick={handleCreate}>Criar</button>
+
+    </body>
   );
 
 }
